7a9070737f8815f70aab8fdacc5ed04bf934367c
ikarus-balsero <osvaldoconcep@gmail.com>
2021-03-17 14:30:28 -0400

    Manipulate webpage to show different temperature and messages

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 // It is always helpful to use comments in your code!
+
+printBoilingTemp = () => {
+  document.getElementById('boiling-temp').innerText = '212';
+};
+
+printFreezingTemp = () => {
+    document.getElementById('freezing-temp').innerText = '32';
+};
+
+printWaterTemp = () => {
+    document.getElementById('water-temp').innerText = '11';
+};
+
+addClass = (elementId, className) => {
+    document.getElementById(elementId).classList.add(className);
+};
+
+removeClass = (elementId, className) => {
+    document.getElementById(elementId).classList.remove(className);
+};
+
+showMessageBasedOnWaterTemperature = () => {
+    setTimeout(() => {
+        const waterTemp = parseInt(document.getElementById('water-temp').innerText);
+        let elementId;
+        if (waterTemp > 212) {
+            elementId = 'boiling';
+        } else if (waterTemp < 32) {
+            elementId = 'frozen';
+        } else {
+            elementId = 'good-temp';
+        }
+        removeClass(elementId, 'hide');
+        addClass(elementId, 'show');
+    }, 3000);
+};
+
+printBoilingTemp();
+printFreezingTemp();
+printWaterTemp();
+showMessageBasedOnWaterTemperature();
